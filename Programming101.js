67ea9c0154c4219403e4c7a2fa8c660e59f15954
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-06-07 11:52:11 +0530

    Namaste DSA

---
+++ Programming101.js
@@ -0,0 +1,45 @@
+'Programming 101 -> Basic'
+
+/* With Strings
+    let firstName = "Akshay";
+    let lastName = "Saini";
+
+    let fullName = firstName + " " + lastName;
+    console.log(fullName);
+*/
+/* With Numbers & Strings
+
+    let firstName = "Akshay";
+    let lastName = 7;
+
+    let fullName = firstName + " " + lastName;
+    console.log(fullName);
+*/
+
+/* Array 
+    let arr = ["akshay", "deepika", "virat", "sachin"];
+    console.log(arr);
+    console.log(arr[0]);    // akshay
+    console.log(arr[2]);    // virat
+    console.log(arr[6]);    // undefined
+*/
+
+/* Object 
+    let obj = {
+        name: "akshay",
+        email: "aka7@gmail.com",
+        contact: 808
+    };
+    console.log(obj);
+    console.log(obj.name);
+    console.log(obj.email);
+    console.log(obj.contact);
+*/ 
+
+/* Mix array & Array of Array */
+let arr = ["xyz", 10, ["raj", "dev"], [1,2,3,4,5], "simran"];
+console.log(arr[1]);        // 10
+console.log(arr[2][1]);     // dev
+console.log(arr[3][3]);     // 4
+console.log(arr[3][2]);     // 3
+console.log(arr[4]);        // simran
