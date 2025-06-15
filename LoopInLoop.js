a5682c522b80803bfb2db704d588db09dc1eb86d
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-06-15 14:17:22 +0530

    Loop In Loop

--- LoopInLoop.js
+++ LoopInLoop.js
@@ -1,6 +1,32 @@
 "Loop in Loop"
-for(let i=0; i<3; i++){
-    for(let j=0; j<3; j++){
-        console.log("first", i, j)
+// for(let i=0; i<3; i++){
+//     for(let j=0; j<3; j++){
+//         console.log("first", i, j)
+//     }
+// }
+
+// for(let i=0; i<3; i++){
+//     for(let j=0; j<i; j++){
+//         console.log(i, j);
+//     }
+// }
+
+// for(let i=0; i<5; i++){
+//     for(let j=0; j<=i; j++){
+//         console.log(i, j);
+//     }
+// }
+
+//6
+// for(let i=0; i<3; i++){
+//     for(let j=i; j>0; j--){
+//         console.log(i, j);
+//     }
+// }
+
+//7
+for(let i=5; i>0; i--){
+    for(let j=0; j<i; j++){
+        console.log(i, j);
     }
 }
