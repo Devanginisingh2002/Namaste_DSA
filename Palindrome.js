12648d5b10098619542806b0ed688716c1166bb2
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-06-20 20:25:52 +0530

    palindrome

--- Palindrome.js
+++ Palindrome.js
@@ -1,13 +1,15 @@
-function Palindrome(num){
-
-    let num = 9832;    // 2389
-    let reverse = 0;
-    while(num > 0){
-        let remain = num % 10;
-        reverse += remain;
-        num = num / 10;
-    }
-   return reverse;
+function palindrome(n) {
+  let reverse = 0;
+  while (n > 0) {
+    let rem = n % 10;
+    reverse = 10 * reverse + rem;
+    n = n / 10;
+  }
+  if (reverse == n) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(Palindrome(num));
+console.log(palindrome(121));
