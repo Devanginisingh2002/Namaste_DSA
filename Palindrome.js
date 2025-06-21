f7e3048416b244057d352de6dee8eb1cabdfa4be
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-06-21 23:40:52 +0530

    file

--- Palindrome.js
+++ Palindrome.js
@@ -1,15 +1,21 @@
 function palindrome(n) {
+  // handle negative case
+  if(n < 0) return false;
+
+  let nCopy = n;
   let reverse = 0;
   while (n > 0) {
     let rem = n % 10;
     reverse = 10 * reverse + rem;
-    n = n / 10;
+    n = Math.floor(n / 10);
   }
-  if (reverse == n) {
+  if (nCopy == reverse) {
     return true;
   } else {
     return false;
   }
 }
 
-console.log(palindrome(121));
+console.log(palindrome(121));       // true
+console.log(palindrome(234));       // false
+console.log(palindrome(-9898));     // false
